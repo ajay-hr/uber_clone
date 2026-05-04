101006113b334601a08fb8394afb4300bade2950
ajay-hr <70533452+ajay-hr@users.noreply.github.com>
2026-05-04 18:24:30 +0530

    Validate DATABASE_URL and configure engine settings
    Added validation for DATABASE_URL and adjusted connection settings for PostgreSQL.

--- database.py
+++ database.py
@@ -1,8 +1,23 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 from app.config import DATABASE_URL
+from fastapi import HTTPException
 
-engine = create_engine(DATABASE_URL)
+if DATABASE_URL and DATABASE_URL.startswith("postgres://"):
+    DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
+
+if not DATABASE_URL:
+    raise HTTPException(
+            status_code=400,
+            detail="Database URL is invalid."
+        )
+
+engine = create_engine(
+    DATABASE_URL,
+    connect_args={"sslmode": "require"}  # required for Supabase
+)
+
+
 SessionLocal = sessionmaker(bind=engine)
 
 Base = declarative_base()
